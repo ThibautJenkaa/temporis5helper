f91070908df71a3d197bf858211f97d00dbbfbc8
Thibaut <thibaut@jenkaa.com>
2021-04-09 14:32:01 +0200

    Signup new users & setup view for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get 'signup', to: "users#new"
+  get 'users', to: "users#new"
+  resources :users, only: [:create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
